172eef48e5ea58bb041d8f0ea5a3818d73a30ce3
Darkmatraque <kevin2007baumann@gmail.com>
2026-04-15 03:49:12 +0200

    Update calendar.js

---
+++ assets/js/calendar.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Calendrier des absences</title>
+  <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
+
+  <h1>Calendrier des absences</h1>
+
+  <div class="calendar-container" id="calendar"></div>
+
+  <script src="../assets/js/calendar.js"></script>
+
+</body>
+</html>
